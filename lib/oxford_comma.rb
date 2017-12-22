ce0d148669091408d82a6fd66c38b02561d6e0a5
Avery Broderick <avery.c.broderick@gmail.com>
2017-12-22 15:20:29 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,7 @@
 def oxford_comma(array)
-  size = array.size
-  case size
-  when array.size == 1
+  case array.size
+  when 1
     return array[0]
-  when array.size == 2
+  when 2
     return array.join(" and ")
 end
